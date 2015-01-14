e68614a4865863e3bd7aec4204bbe448b9811b6b
Peter Hurford <peter@peterhurford.com>
2015-01-14 13:05:44 -0600

    typofixes in GitHub validation

--- app/api/gitpiv/api.rb
+++ app/api/gitpiv/api.rb
@@ -23,8 +23,7 @@ module Gitpiv
       github_branch = params['pull_request']['head']['ref']
       github_action = params['action']
       error!('No action', 500) unless github_action.present?
-      error!('No body', 500) unless github_body.present?
-      error!('No branch', 500) unless github_body.present?
+      error!('No branch', 500) unless github_branch.present?
       pivotal_id = find_pivotal_id(github_body, github_branch)
       {
         action: github_action,
