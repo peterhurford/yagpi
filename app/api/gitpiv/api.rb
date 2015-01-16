57f456147b81582feaee6f51db3fc187a1653d0d
Peter Hurford <peter@peterhurford.com>
2015-01-16 11:27:17 -0600

    fix Pivotal API endpoint

--- app/api/gitpiv/api.rb
+++ app/api/gitpiv/api.rb
@@ -13,6 +13,7 @@ module Gitpiv
       end
 
       def connect_to_pivotal!
+        ENV['PIVOTAL_API_KEY'] = 'a25b06a7faa284b78f99a1e3d18350c1'
         error!('PIVOTAL_API_KEY not set', 500) unless ENV['PIVOTAL_API_KEY'].present?
         @pivotal_conn ||= RestClient::Resource.new("https://www.pivotaltracker.com/services/v5", :headers => {'X-TrackerToken' => ENV['PIVOTAL_API_KEY'], 'Content-Type' => 'application/json'})
       end
@@ -29,7 +30,7 @@ module Gitpiv
       def change_story_state!(pivotal_id, github_pr_url, github_author, pivotal_action)
         connect_to_pivotal!
         pivotal_verb = (pivotal_action == 'finished' ? "Finishes" : "Delivers")
-        @pivotal_conn["source_commit"].post("{'source_commit':{'commit_id':'','message':'[#{pivotal_verb} ##{pivotal_id}] #{pivotal_action.capitalize} via YAGPI GitHub Webhook.','url':'#{github_pr_url}','author':'#{github_author}'}}")
+        @pivotal_conn["source_commits"].post('{"source_commit":{"commit_id":"","message":"[' + pivotal_verb + ' #' + pivotal_id + '] ' + pivotal_action.capitalize + ' via YAGPI GitHub Webhook+","url":"' + github_pr_url + '","author":"' + github_author + '"}}')
       end
 
       def nag_for_a_pivotal_id!(github_pr_url)
